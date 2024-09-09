c1cae43db19d95fb2254abc04d1e2caaed1b3cb4
thales <thaleswcm@estudante.ufscar.br>
2024-09-08 23:11:24 -0300

    ajustes nos comentarios

--- src/main/java/br/ufscar/dc/compiladores/compilador/receitas/TabelaDeSimbolos.java
+++ src/main/java/br/ufscar/dc/compiladores/compilador/receitas/TabelaDeSimbolos.java
@@ -1,9 +1,6 @@
 package br.ufscar.dc.compiladores.compilador.receitas;
 
-
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class TabelaDeSimbolos {
@@ -13,18 +10,15 @@ public class TabelaDeSimbolos {
         INSTRUCAO
     }
 
-    private final Map<String, TipoSimbolo> mapaSimbolos = new HashMap<>();
     public final Set<String> ingredientes = new HashSet<>();
     public final Set<String> instrucoes = new HashSet<>();
 
     public void adicionarIngrediente(String nome) {
         ingredientes.add(nome);
-        mapaSimbolos.put(nome, TipoSimbolo.INGREDIENTE);
     }
 
     public void adicionarInstrucao(String instrucao) {
         instrucoes.add(instrucao);
-        mapaSimbolos.put(instrucao, TipoSimbolo.INSTRUCAO);
     }
 
     public boolean ingredienteExiste(String nome) {
